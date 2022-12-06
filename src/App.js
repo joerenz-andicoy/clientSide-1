39a62f1df2d3f5f13b434e16305388c1feaff65c
JOSE_LORENZO_ANDICOy <andicoylorenzo@gmail.com>
2022-12-06 08:28:10 +0800

    git client-side commit message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+  const [movieName, setMovieName] = useState("");
+  const [review, setReview] = useState("");
+  const [movieReviewList, setMovieReviewList] = useState([]);
+  const [newReview, setNewReview] = useState("");
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/api/get").then((response) => {
+      setMovieReviewList(response.data);
+    });
+  }, []);
+
+  const submitReview = () => {
+    Axios.post("http://localhost:3001/api/insert", {
+      movieName: movieName,
+      movieReview: review,
+    });
+    setMovieReviewList([
+      ...movieReviewList,
+      {
+        movieName: movieName,
+        movieReview: review,
+      },
+    ]);
+  };
+
+  const deleteReview = (movie) => {
+    Axios.delete(`http://localhost:3001/api/delete/${movie}`);
+  };
+
+  const updateReview = (movie) => {
+    Axios.put("http://localhost:3001/api/update", {
+      movieName: movie,
+      movieReview: newReview,
+    });
+    setNewReview("");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Banana-Rater!</h1>
+      <div className="form">
+        <label>Movie Name:</label>
+        <input
+          className="box-input"
+          type="text"
+          name="movieName"
+          onChange={(e) => setMovieName(e.target.value)}
+        />
+        <label>Review:</label>
+        <input
+          className="box-input"
+          type="text"
+          name="review"
+          onChange={(e) => setReview(e.target.value)}
+        />
+        <button onClick={submitReview} className="submit-button">
+          Submit
+        </button>
+
+        {movieReviewList.map((val) => {
+          return (
+            <div className="card">
+              <h1>{val.movieName}</h1>
+              <p>{val.movieReview}</p>
+
+              <button
+                onClick={() => {
+                  deleteReview(val.movieName);
+                }}
+              >
+                Delete
+              </button>
+              <input
+                type="text"
+                id="updateInput"
+                onChange={(e) => {
+                  setNewReview(e.target.value);
+                }}
+              />
+              <button
+                onClick={() => {
+                  updateReview(val.movieName);
+                }}
+              >
+                Update
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
